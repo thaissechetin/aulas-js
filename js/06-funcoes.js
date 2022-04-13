a932127fb9fd86c9ecf83b7375595d75f459346a
Thaís Sechetin <sechetin.thais@gmail.com>
2022-04-13 14:18:52 -0300

    exemplos de sintaxes das funções
    sintaxe tradicional e nova, arrow function e exemplo de arrow com dois parâmetros.

--- js/06-funcoes.js
+++ js/06-funcoes.js
@@ -27,11 +27,93 @@ linha()
 
 
 //parâmetros/argumentos
-function soma (valor1, valor2){
-    let total = valor1 +valor2;
+//sintaxe tradicional 
+/* function soma (valor1, valor2){
+    let total = valor1 + valor2;
     return total; //variável local, que só está dentro da função
 }
+ */
 
-console.log( soma(10, 5) );
+//versão 2:sintaxe ARROW FUNCTION (FUNÇÃO SETA/FLECHA)
+const soma = (valor1, valor2) => {
+    let total = valor1 + valor2;
+    return total;
+};
+
+//arrow function com retorno IMPLÍCITO
+const soma1 = (valor1, valor2) => valor1 + valor2;
+
+
+
+
+console.log( soma1(18, 5) );
+console.log( soma1(100, 50));
+console.log( soma1(15, 60));
+
 
 /* função é código que se reaproveita, procedimento não dá retorno, função sim.  */
+
+linha();
+
+console.log("13/04/2022")
+
+linha();
+
+//versão 1: tradicional
+/* function dobra(numero){
+    let resultado =  numero * 2;
+    return  resultado;
+} */
+
+//ou
+
+/* function dobra(numero){
+    return numero * 2;
+} */
+
+
+
+//Versão 2: arrow function
+/* const dobra = (numero) => {
+    let resultado = numero * 2;
+    return resultado;
+
+};   */
+//os parenteses sempre são o parametro da função e as chaves sempre entram as variaveis
+
+/* const dobra = (numero) => {
+    return numero * 2;
+} */
+
+
+//retorno implícito (remove chaves e return)
+//OBS: Como só temos um parâmetro  (número), pode tirar os ()
+const dobra = numero => numero * 2;
+
+
+
+console.log(dobra(20));
+console.log(dobra(100));
+
+linha();
+
+let nota1 =8.5;
+let nota2 =7.4;
+
+const calculaMedia = (n1, n2) =>  (n1 + n2)/2;
+console.log(calculaMedia(nota1, nota2));
+
+
+//obs: se não tem parâmetros, também precisa usar ()
+const linha2 = () => console.log("**************")
+
+
+
+
+
+
+
+
+
+
+
